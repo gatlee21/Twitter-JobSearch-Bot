0f64a7c9b8817b8eecd01eee0462ef6d24b32bf4
atlee19 <atleegraham16@gmail.com>
2020-11-09 13:34:52 -0500

    added ensure login to /home route but need a cleaner implementation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home' });
-});
+router.get('/home', 
+  require('connect-ensure-login').ensureLoggedIn('/'), 
+  function(req, res, next) {
+    res.render('home', { title: 'Home' })
+})
 
 module.exports = router;
