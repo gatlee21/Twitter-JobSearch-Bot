a38a904a9ee64eaefb020e7b01cbed9c40f76151
atlee19 <atleegraham16@gmail.com>
2020-11-09 23:11:02 -0500

    temp removed ensure login until reroute bug can be solved

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
-// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
+// const connect = require('connect-ensure-login')
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-router.get('/home', 
-  require('connect-ensure-login').ensureLoggedIn('/'), 
-  function(req, res, next) {
+router.get('/home', function(req, res, next) {
     res.render('home', { title: 'Home' })
 })
 
