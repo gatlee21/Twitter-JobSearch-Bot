bb221216bea9fc984f4de9077a2e19ee7a193dbb
atlee19 <atleegraham16@gmail.com>
2020-11-11 09:23:18 -0500

    twit client finds and sends match data

--- src/routes/twitter.client.js
+++ src/routes/twitter.client.js
@@ -2,12 +2,42 @@ const express = require('express');
 const router = express.Router();
 const T = require('../helpers/twit.config')
 
+const home_timeline = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
+
+const match_data = []
+
 router.get('/get/home_timeline', function(req, res){
 
-    T.get('search/tweets', { q: 'Work', count: 100 }, function(err, data, response) {
-        console.log(data)
+    T.get(home_timeline, { count: 5 }, function(err, data){
+        for(x in data){
+            if(data[x].text.includes('hiring')){
+                let log_data = {
+                    name : data[x].user.name,
+                    text : data[x].text
+                }
+                match_data.push(log_data)
+            }
+    
+        }
+        res.json(match_data)
     })
 })
 
+// function crawl(err, data){
+//     for(x in data){
+//         if(data[x].text.includes('hiring')){
+//             let log_data = {
+//                 name : data[x].user.name,
+//                 text : data[x].text
+//             }
+//             match_data.push(log_data)
+//         }
+
+//         // console.log(data[x].user.name)
+//         // console.log(data[x].entities.urls) 
+//     }
+//     console.log('match data:')
+//     console.log(match_data)
+// }
 
 module.exports = router
